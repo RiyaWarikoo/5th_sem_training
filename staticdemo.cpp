40634305c7073c5ded96c06d945c22cf21af89d2
Riya Warikoo <121093317+RiyaWarikoo@users.noreply.github.com>
2024-07-19 13:20:53 +0530

    Add files via upload

---
+++ staticdemo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    auto divide=[](int a, int b) -> double {
+        if(b==0) {
+            return 0;
+        }
+        return static_cast<double>(a)/b;
+    };
+    cout<<"division result: "<<divide(10,2)<<endl;
+    cout<<"division by zero result: "<<divide(10,0)<<endl;
+
+    return 0;
+}
